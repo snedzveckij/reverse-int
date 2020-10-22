3d1a7e800a1e355130f138534cf111f7bfb4d4da
Stanislav Nedzvetskiy <snedzveckij@gmail.com>
2020-10-22 18:24:50 +0300

    feat: implement reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+      arr[i] = str[i];
+    if(arr[i] === '-') {
+         arr.splice(i, 1);
+      }
+    }
+    return arr.reverse().join('');
 }
